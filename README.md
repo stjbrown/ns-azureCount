fcf2fbef03186c53851a528808c02164469aa622
Stephen Brown <stjbrown@gmail.com>
2018-11-19 14:37:59 +0000

    Updated script and README

---
+++ README.md
@@ -0,0 +1,13 @@
+# Netskope Azure Count Script
+
+To run this script log into [Azure Portal](https://portal.azure.com) and click on the cloud shell button.
+
+Switch to the bash shell if not already there.
+
+Run 
+
+```
+git clone https://github.com/stjbrown/ns-azureCount.git
+cd ns-azureCount
+./azureCount.sh
+```
--- azureCount.sh
+++ azureCount.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
+VMCOUNT=$(az resource list | jq " .[] .type" | grep -c -w '"Microsoft.Compute/virtualMachines"')
+SQLCOUNT=$(az resource list | jq " .[] .type" | grep -c '"Microsoft.Sql/servers/databases"')
+COSMOSCOUNT=$(az cosmosdb list | jq " . | length")
+FUNCTIONAPPS=$(az functionapp list | jq " . | length")
+CONTAINERCOUNT=0
 
-vmCount=$(az resource list | jq " .[] .type" | grep -c -w '"Microsoft.Compute/virtualMachines"')
+for row in $(az storage account list | jq -r ".[] | @base64"); do
+  _jq() {
+                echo ${row} | base64 --decode | jq -r ${1}
 
-blobCount=$(az storage account list | jq " .[] .kind" | grep -c '"BlobStorage"')
+  }
+ if [ $(_jq '.kind') = "BlobStorage" ]; then
+     NEWCOUNT="$(az storage container list --account-name $(_jq '.name') | jq '. | length')"
+     CONTAINERCOUNT=$(($CONTAINERCOUNT + $NEWCOUNT))
+fi
+done
 
-sqlDbs=$(az resource list | jq " .[] .type" | grep -c '"Microsoft.Sql/servers/databases"')
 
-cosmosDbs=$(az cosmosdb list | jq " .[] .provisioningState" | grep -c "Succeeded")
 
-functionApps=$(az functionapp list | jq " .[] .kind" | grep -c "functionapp")
 
-echo "VM Count: $vmCount"
-echo "Blob Storage Count: $blobCount"
-echo "SQL DB Count: $sqlDbs"
-echo "Cosmos DB Count: $cosomosDbs"
-echo "Functions App Count: $functionApps"
+
+
+echo "VM Count: $VMCOUNT"
+echo "Blob Storage Containers: $CONTAINERCOUNT"
+echo "SQL DB Count: $SQLCOUNT"
+echo "Cosmos DB Count: $COSMOSCOUNT"
+#echo "Functions App Count: $FUNCTIONAPPS"
